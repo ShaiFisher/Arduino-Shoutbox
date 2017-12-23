5af83dfaccd856a0448552a9e11f1238c1bb6c18
Shai Fisher <shaif@balink.net>
2017-12-23 20:15:49 +0200

    functions for wait, documentation

--- shoutbox.ino
+++ shoutbox.ino
@@ -5,21 +5,21 @@
 #include "Switchable.h"
 #include "Button.h"
 
-
 // Pin Definitions
-#define LDR_PIN_SIG	A3
-#define LED_PIN  3
-#define PUSHBUTTON_PIN  4
-#define RECORD_PIN  11
-#define PLAY_PIN  13
-
-
+// https://www.circuito.io/static/reply/index.html?solutionId=5a3d4c6bfb33e200225be712&solutionPath=storage.circuito.io
+// https://www.elabpeers.com/isd1820-voice-recording-module.html
+#define LDR_PIN_SIG	A3      // with 10k ohm resistor
+#define LED_PIN  3          // with 330 ohm resistor
+#define PUSHBUTTON_PIN  4   // with 10k ohm resistor
+#define RECORD_PIN  11      // to ISD1820 
+#define PLAY_PIN  13        // to ISD1820 
 
 // Global variables and defines
 #define THRESHOLD_ldr   100
-#define DURATION 5000
-//int ldrAverageLight;
-int wasDark = 0;
+#define MAX_DURATION 5000
+#define LIGHT_LIMIT 250
+int duration;
+int wasDark;
 // object initialization
 LDR ldr(LDR_PIN_SIG);
 LED led(LED_PIN);
@@ -52,19 +52,16 @@ void loop()
         record();
     }
 	// read ldr
-    int ldrSample = ldr.read();
+    /*int ldrSample = ldr.read();
     Serial.print(F("Light Sample: "));
-    Serial.println(ldrSample);
+    Serial.println(ldrSample);*/
 	
-    if (ldrSample > 180) {
-      // light
+    if (isLight()) {
       if (wasDark) {
         Serial.println("light");
-		// light led
-        led.on();
-		play();
-        led.off();
-		// wait until box is closed
+		    play();
+		    // wait until box is closed
+        waitUntillClosed();
         wasDark = 0;
       }
     } else {
@@ -73,27 +70,78 @@ void loop()
       //digitalWrite(LED_PIN, LOW);
       //digitalWrite(PLAY_PIN, LOW);
       wasDark = 1;
-      delay(1000);
+      //delay(1000);
     }
-    
+    delay(500);
+}
+
+int isDark() {
+  int ldrSample = ldr.read();
+  return (ldrSample < LIGHT_LIMIT);
+}
+
+int isLight() {
+  return !isDark();
+}
+
+void waitUntillOpened() {
+  log("waitUntillOpened: start");
+  while (isDark()) {
+    log("waitUntillOpened: wait");
+    delay(500);
+  }
+  log("waitUntillOpened: end");
+}
+
+int waitUntillClosed() {
+  log('waitUntillClosed: start');
+  int duration = 0;
+  while (isLight()) {
+    log('waitUntillClosed: wait');
+    delay(500);
+    duration += 500;
+  }
+  log('waitUntillClosed: end');
+  return duration;
 }
 
 void record() {
+  log("record");
+  waitUntillOpened();
 	digitalWrite(RECORD_PIN, HIGH);
-	delay(DURATION);
+  int duration = waitUntillClosed();
+  log("duration", duration);
+	//delay(MAX_DURATION);
 	digitalWrite(RECORD_PIN, LOW);
 }
 
 void play() {
 	// play
+  led.on();
 	digitalWrite(PLAY_PIN, HIGH);
 	// shut play pin (but continue play)
 	delay(100);
 	digitalWrite(PLAY_PIN, LOW);
 	// wait until play is over
-	delay(DURATION);
+	delay(duration);
+  led.off();
 }
 
+void log(String str) {
+  Serial.println(str);
+}
+
+void log(String desc, int num) {
+  Serial.print(desc);
+  Serial.print(": ");
+  Serial.println(num);
+}
+
+void log(String str1, int num, String str2) {
+  Serial.print(str1);
+  Serial.print(num);
+  Serial.println(str2);
+}
 /*******************************************************
 
 *    Circuito.io is an automatic generator of schematics and code for off
